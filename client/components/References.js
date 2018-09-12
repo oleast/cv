eab1def7f461e118df866908bf1dd60c06e70361
oleast <oleast@stud.ntnu.no>
2018-09-12 14:49:39 +0200

    Start working on lit-element implementation

--- client/components/References.js
+++ client/components/References.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react'
+//import React, { Component } from 'react'
+import { LitElement, html } from '@polymer/lit-element';
 
-const References = ({ references }) => (
-    <div className="section section-references">
-        <h2 className="title">{ references.title }</h2>
+class References extends LitElement {
+  render() {
+    return html`
+      <div className="section section-references">
+        <h2 className="title">${ references.title }</h2>
         <div className="set">
-            <div className="item">
-                <span className="info">
-                    { references.values.map((reference) => (
-                        <div key={reference.name}>
-                            { reference.name }: { reference.value }
-                        </div>
-                    ))}
-                </span>
-            </div>
+          <div className="item">
+            <span className="info">
+              ${ references.values.map((reference) => (
+                html`<div key={reference.name}>
+                  ${ reference.name }: ${ reference.value }
+                </div>`
+              ))}
+            </span>
+          </div>
         </div>
         <div className="clear"> </div>
-    </div>
-)
+      </div>
+    `
+  }
+}
+
+customElements.define('references', References);
 
 export default References
